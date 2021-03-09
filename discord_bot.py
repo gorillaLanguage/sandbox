4e114622c334d152be184c8e95c7e80c0b4d2f45
gorillaLanguage <tkzm50@gmail.com>
2021-03-09 22:56:19 +0900

    うんこ追加 メッセージを配列へ

--- discord_bot.py
+++ discord_bot.py
@@ -17,10 +17,17 @@ async def on_ready():
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
+    
+    messageList = {
+        'neko':'ﾆｬｰﾝ'
+        ,'うんこ':'ｳﾝｺｫｫｫｫｫｫｫｫｫ!!!!!'
+        ,'うんこよし！':'ｳﾝｺﾖｼ!'
+        }
 
     # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+    if message.content in messageList:
+        await message.channel.send(messageList[message.content])
+
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
