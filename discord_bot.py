70790c907c62404518258d66c4a7b7da53a7f168
gorillaLanguage <tkzm50@gmail.com>
2021-03-09 23:23:12 +0900

    起動時メッセージ追加

--- discord_bot.py
+++ discord_bot.py
@@ -5,6 +5,9 @@ import sys
 # 自分のBotのアクセストークンに置き換えてください
 TOKEN = sys.argv[1]
 
+# チャンネルのID
+CHANNELID = 767327887719399446
+
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
@@ -12,22 +15,30 @@ client = discord.Client()
 @client.event
 async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+    print('login')
+    await oha()
 
-# メッセージ受信時に動作する処理
+async def oha():
+    channel = client.get_channel(CHANNELID)
+    await channel.send('ﾏｲﾝｸﾗﾌﾄｻｰﾊﾞｰ 管理ﾎﾞｯﾄ ﾀﾞﾖ')
+    await channel.send('ﾎﾞｸｶﾞｵﾝﾗｲﾝﾉ時 ﾊ ｻｰﾊﾞｰ起動中 ﾀﾞﾖ')
+    await channel.send('ｿﾉｳﾁ 色々ﾃﾞｷﾙﾖｳﾆﾅﾙ ｹﾄﾞ 今ﾊｳﾝｺﾎﾞｯﾄ ﾀﾞﾖ')
+
+
+#メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
     
+    #キーワードリスト
     messageList = {
         'neko':'ﾆｬｰﾝ'
         ,'うんこ':'ｳﾝｺｫｫｫｫｫｫｫｫｫ!!!!!'
         ,'うんこよし！':'ｳﾝｺﾖｼ!'
         }
 
-    # 「/neko」と発言したら「にゃーん」が返る処理
+    #ライブラリからキーワード持ってきて返す
     if message.content in messageList:
         await message.channel.send(messageList[message.content])
-
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
