c6d6e239c35bf3c20e60f3a4930cd2496c998cc9
gorillaLanguage <52670973+gorillaLanguage@users.noreply.github.com>
2022-06-06 08:58:16 +0900

    cs

---
+++ cs/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace cs{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello");
+        }
+    }
+}
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// https://docs.microsoft.com/ja-jp/dotnet/core/tutorials/with-visual-studio-code?pivots=dotnet-6-0
